8b606b18c02d0af8db280e0ecaf3365f67b82408
Kamil Szwaradzki <kszwaradzki@cairo.pl>
2021-02-16 15:20:13 +0100

    style: change wrong id & change padding float to flex

--- public/js/theme_changer.js
+++ public/js/theme_changer.js
@@ -1,5 +1,5 @@
 $("#custom-styles").on("change",function() {
    var selected_style = $("#custom-styles option:selected").attr('value');
-   var current_style = $("#custom-variables").attr('href');
+   var current_style = $("#custom-theme").attr('href');
    $("#custom-theme").attr('href', current_style.replace(/(css\/).*?(\.css)/,"$1" + selected_style + "$2"));
 });
